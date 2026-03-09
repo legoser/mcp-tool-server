aa19aa2ef825bc2c373bb3b81cc4880e80156199
Legos Dev <legos.creator@gmail.com>
2026-03-09 07:09:05 +0700

    [upd] move prompt

--- src/tools/time_tools.py
+++ src/tools/time_tools.py
@@ -7,6 +7,6 @@ logger = get_logger(__name__)
 
 async def get_current_time() -> str:
     """Возвращает текущую дату и время по Москве (UTC+3)."""
-    moscow_tz = timezone(timedelta(hours=3))
+    moscow_tz = timezone(timedelta(hours=3))  # TODO get user timezone
     now = datetime.now(moscow_tz)
-    return now.strftime("%d.%m.%Y %H:%M:%S (МСК)")
+    return now.strftime("%d.%m.%Y %H:%M:%S (МСК)")  # TODO format based on user locale
